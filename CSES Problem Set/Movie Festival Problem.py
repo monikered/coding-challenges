c3dda13010d0e632abdbe80d33f0198711f59987
monikered <monica.remmers@gmail.com>
2021-01-18 22:30:43 +0000

    used more informative function names

--- CSES Problem Set/Movie Festival Problem.py
+++ CSES Problem Set/Movie Festival Problem.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Movie Festival Problem (from CSES)
-
-# In[14]:
-
-
 # from https://cses.fi/problemset/task/1629/
-# input n is number of movies
-# maximize n given start and end times for each movie
+# input is number of movies
+# maximize movies one can watch given start and end times for each movie
 
 import numpy as np
 
-def main():
+def input_handler():
     movies = [int(x) for x in input().split()] 
     times = np.reshape(movies[1:], (-1, 2))
-    scheduler(times)
+    return times
 
-def scheduler(times):
+def scheduler(schedule):
     can_watch = 0
-    while times.size > 0:
-        if times.shape[0] == 1: # number of movies in list
+    while schedule.size > 0:
+        if schedule.shape[0] == 1: # number of movies in list
             can_watch += 1
             break
         else:
-            x = np.amin(times[:,1]) # find movie that ends earliest
+            x = np.amin(schedule[:,1]) # find movie that ends earliest
             can_watch += 1
-            times = times[~np.any(times < x, axis=1), :] # remove it and its intersections from list
+            schedule = schedule[~np.any(schedule < x, axis=1), :] # remove x and its intersections from list
     return can_watch
 
 if __name__ == '__main__':
-    main()
-    print(can_watch)
-
+    times = input_handler()
+    print(scheduler(times))
